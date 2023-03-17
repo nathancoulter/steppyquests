05c06935378208460ceb39d54d075b85408d841c
Nathancoulter <nathan@coulter.ninja>
2023-03-17 13:48:24 -0400

    Getting to a clean commit. Created Scene_controller, scene_helper and scene_Controller quest

---
+++ app/controllers/scenes_controller.rb
@@ -0,0 +1,12 @@
+class ScenesController < ApplicationController
+    def create
+        @quest = Quest.find(params[:quest_id])
+        @scene = @quest.scenes.create(scene_params)
+        redirect_to quest_path(@quest)
+      end
+    
+      private
+        def scene_params
+          params.require(:scene).permit(:title, :body)
+        end
+    end
